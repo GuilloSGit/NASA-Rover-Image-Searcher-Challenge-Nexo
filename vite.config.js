4de4cc8656c7d293c24cdd35190ba60baffbaadd
Guillermo Andrada <guillermoandrada@gmail.com>
2023-07-29 14:32:52 -0300

    Deployar desde carpeta dist del npm gh-pages
    El deploy por medio del workflow no funcionó, por eso se rehizo el trabajo [ Deploy: prueba 2]

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '',
+  base: '/NASA-Rover-Image-Searcher-Challenge-Nexo/',
   plugins: [react()],
 })
